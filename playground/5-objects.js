da142be7403023d61a1faf20fa9ca574fbfb67f1
aysuakbaba <aysuakbabaa@gmail.com>
2023-03-08 13:37:22 +0300

    Object Destructuring

---
+++ playground/5-objects.js
@@ -0,0 +1,14 @@
+//OBJECT DESTRUCTURING
+const product = {
+    label: 'Red notebook',
+    price:3,
+    stock:201 ,
+    salePrice: undefined,
+    rating: 4.2
+}
+
+//const label = product.label
+//const stock = product.stock
+
+
+//const {label:productLabel, stock, rating= 5} = product
