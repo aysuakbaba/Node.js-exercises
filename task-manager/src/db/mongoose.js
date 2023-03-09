576515bcc5c6bca0b2d4f681749609b26813e585
aysuakbaba <aysuakbabaa@gmail.com>
2023-03-09 12:47:30 +0300

    Data Validation and Sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,22 +1,60 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 
 const User = mongoose.model('User', {
     name:{
-        type:String
+        type:String,
+        required: true,
+        trim: true
 
     },
+    email:{
+        type:String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+
+        }
+    },
     age:{
-        type:Number
+        type:Number,
+        default: 0,
+        validate(value) {
+            if(value<0){
+                throw new Error('Age must be a positive')
+            }
+
+        }
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value) {
+            if(value.toLowerCase().includes('password')){
+                throw new Error("Password can't be password")
+        }
     }
+}
+
 })
 
 
+
 // const me = new User({
-//     name: 'Aysu',
-//     age: 'a'
+//     name: '   Aysu',
+//     email: 'aysuakbabAA@gmail.COM   ',
+//     age: 22,
+//     password: 'aysuaaa'
+
 // })
 
 
@@ -31,17 +69,20 @@ const User = mongoose.model('User', {
 
 const Task = mongoose.model('Task', {
     description:{
-        type:String
+        type:String,
+        trim:true,
+        required:true
     },
     completed:{
-        type:Boolean
+        type:Boolean,
+        default:false
     }
 })
 
 
 const taskOne = new Task({
-   description: 'Learn Mongoose',
-   completed: false
+    description:'   Learn Mongoose'
+   
 })
 
 
