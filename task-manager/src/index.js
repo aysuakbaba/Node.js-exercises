5beb5468531073268178b6e0ac1ade1c0a695e51
aysuakbaba <aysuakbabaa@gmail.com>
2023-03-09 17:38:43 +0300

    Resource Reading Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,6 +21,33 @@ app.post('/users',(req, res)=> {
     })
 })
 
+app.get('/users', (req, res)=>{
+    User.find({}).then((users)=> {
+        res.send(users)
+
+    }).catch((e) => {
+        res.status(500).send(e)
+
+    })
+})
+
+//for a specific user to find:
+app.get('/users/:id', (req,res)=> {
+    const _id = req.params.id
+    User.findById(_id).then((user)=> {
+        if(!user){
+            return res.status(400).send
+        }
+
+        res.send(user)
+
+    }).catch((e) => {
+        res.status(500).send()
+
+    })
+
+})
+
 app.post('/tasks', (req, res)=> {
     const task = new Task(req.body)
     task.save().then(()=> {
@@ -28,6 +55,31 @@ app.post('/tasks', (req, res)=> {
 
     }).catch((e)=>{
         res.status(400).send(e)
+
+    })
+})
+
+app.get('/tasks', (req,res)=> {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+
+    }).catch((e) => {
+        res.status(500).send(e)
+
+    })
+})
+
+app.get('/tasks/:id', (req,res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(400).send
+        }
+
+        res.send(task)
+
+    }).catch((e)=> {
+        res.status(500).send(e)
 
     })
 })
