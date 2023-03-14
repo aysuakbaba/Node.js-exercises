81d6944c6fd1d9dbe2921b8c0ce80af1565bb5ae
aysuakbaba <aysuakbabaa@gmail.com>
2023-03-14 14:27:53 +0300

    Express Middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,6 +8,24 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+//Without middleware: new request --> run route handler
+//With middleware: new request --> do something(function etc.) --> run route handler
+
+// app.use((req, res, next) => {
+//     if(req.method === 'GET'){
+//         res.send('GET requests are disabled')
+
+//     }else{
+//         next()
+//     }
+
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('Service Unavailable')
+
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
