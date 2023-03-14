b151f73569ab393262077e25ba6026fab1896606
aysuakbaba <aysuakbabaa@gmail.com>
2023-03-14 10:53:15 +0300

    Logging In Users

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -44,6 +44,18 @@ router.get('/users/:id',async (req,res) => {
 
 })
 
+router.post('/users/login', async(req,res) => {
+    try{
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+
+    } catch(e){
+        res.status(400).send()
+
+    }
+
+})
+
 router.patch('/users/:id', async(req,res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'age', 'password']
@@ -52,7 +64,14 @@ router.patch('/users/:id', async(req,res) => {
         return res.status(400).send({error: 'Invalid updates!'})
     }
     try{
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true , runValidators:true})
+        //These 3 line of code below is for UserSchema to work properly.
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update] )
+        await user.save()
+
+
+        //commented the line below because UserSchema doesn't work properly.
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true , runValidators:true})
         if(!user){
             return res.status(404).send()
         }
